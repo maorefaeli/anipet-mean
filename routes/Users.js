68d8781ff28ad809e6ed66b2544293b4ec74d1e6
Maor Refaeli <maor.refaeli@servicenow.com>
2020-02-14 14:52:21 +0200

    refactor user

--- routes/Users.js
+++ routes/Users.js
@@ -11,44 +11,49 @@ router.get('/test', (req, res) => res.json({"msg": "Users works"}));
 // @desc Register user
 // @access Public
 
-router.post('/register', (req, res) => {
-    User.findOne({ name: req.body.name })
-        .then(user => {
-            if (user) {
-                return res.status(400).json({"error":"user already exist"});
-            } else {
-                const newUser = new User({
-                    name: req.body.name,
-                    password: req.body.password,
-                    isAdmin: req.body.isadmin
-                });
-                newUser.save()
-                    .then(user => res.json(user))
-                    .catch(err => console.log(err))
-            }
+router.post('/register', async (req, res) => {
+    let user = await User.findOne({ name: req.body.name });
+
+    if (user) {
+        return res.status(400).json({"error": "user already exist"});
+    } else {
+        user = new User({
+            name: req.body.name,
+            password: req.body.password,
+            isAdmin: req.body.isAdmin
         });
+
+        try {
+            user = await user.save();
+            res.json(user);
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({"error": "problem saving user"})
+        }
+    }
 });
 
 // @route POST api/users/login
 // @desc Login user
 // @access Public
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     const name = req.body.name;
     const password = req.body.password;
-    //find user by name
-    User.findOne({ name })
-        .then(user => {
-            //Check for user
-            if(!user) {
-                return res.status(404).json({"error":"user not found"});
-            }
-            if (password == user.password) {
-                return res.status(200).json({"success":"User found"})
-            } else {
-                return res.status(400).json({"error":"wrong password"})
-            }
-        });
+
+    // find user by name
+    const user = await User.findOne({ name });
+
+    // Check for user
+    if (!user) {
+        return res.status(400).json({"error": "User not found"});
+    }
+
+    if (password !== user.password) {
+        return res.status(400).json({"error": "wrong password"});
+    }
+
+    return res.status(200);
 });
 
 module.exports = router;
