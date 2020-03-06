080f52f21d2cec863bce23fdd75ceda5b2eefe2b
Maor Refaeli <maor.refaeli@servicenow.com>
2020-03-06 14:11:07 +0200

    Add services for stores and purchases

---
+++ client/src/app/_services/purchase.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import Purchase from '../_models/purchase';
+
+@Injectable({ providedIn: 'root' })
+export class PurchaseService {
+    constructor(private http: HttpClient) { }
+
+    private httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+    get() {
+        return this.http.get<Purchase[]>('api/purchases/get');
+    }
+
+    add(productId: string) {
+        return this.http.post<Purchase>('api/purchases/add', JSON.stringify({ productId }), this.httpOptions);
+    }
+}
