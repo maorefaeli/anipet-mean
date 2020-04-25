8344910112fd8c9ff8a50f8d8b397dd5f5528990
Kobi Avshalom <kob.a@develeap.com>
2020-04-25 19:22:40 +0300

    added route

--- routes/Prediction.js
+++ routes/Prediction.js
@@ -8,43 +8,44 @@ const User = require('../models/User');
 const Purchase = require('../models/Purchase');
 
 async function predict([userPurchases], [products]) {
-    const SVM = await
-    require('libsvm-js');
-    const svm = new SVM({
-        kernel: SVM.KERNEL_TYPES.LINEAR,    // The type of kernel I want to use
-        type: SVM.SVM_TYPES.C_SVC,          // The type of SVM I want to run
-        gamma: 1,                           // RBF kernel gamma parameter
-        cost: 1                             // C_SVC cost parameter
-    });
+    var svm = require('node-svm');
 
-    // This is the xor problem
-    //
-    //  1  0
-    //  0  1
-    const features = [["Dog Vegan", "Dog Food"], ["Cat Toy"], ["Dog Toy"], ["Cat Salmon"]];
-    const labels = ["Dog Snacks", "Cat Salmon", "Dog Snacks", "Cat Snacks"];
-    svm.train(features, labels);  // train the model
-    const predictedLabel = svm.predictOne(products);
-    console.log(predictedLabel) // 0
+    var xor = [
+        [[0, 0], 0],
+        [[0, 1], 1],
+        [[1, 0], 1],
+        [[1, 1], 0]
+    ];
+
+    var clf = new svm.CSVC();
+    clf.train(xor).done(function () {
+        // predict things
+        xor.forEach(function(ex){
+            var prediction = clf.predictSync(ex[0]);
+            console.log('%d XOR %d => %d', ex[0][0], ex[0][1], prediction);
+        });
+    });
+    //const predictedLabel = svm.predictOne(products);
+    //console.log(predictedLabel) // 0
 }
 
-xor().then(() => console.log('done!'));
+router.post('/predict', async (req, res) => {
+    const {userId} = req.body;
+    try {
+        userPurchases = await Purchase.find();
+        userPurchases = userPurchases.filter( function(userPurchase){return (userPurchase.userId===userId);} );
 
-// router.post('/predict', async (req, res) => {
-//     const {userId} = req.body;
-//     try {
-//         userPurchases = await Purchase.find();
-//         userPurchases = userPurchases.filter( function(userPurchase){return (userPurchase.userId===userId);} );
-
-//         products = await Product.find();
-//         if(!products){
-//             return res.status(400).json({"error":"There are not products"});
-//         }
-//         return res.json(products);
-//     } catch (error) {
-//         console.log(error);
-//         res.status(400).json({"error":"Problem getting products"})
-//     }
-// });
+        products = await Product.find();
+        if(!products){
+            return res.status(400).json({"error":"There are not products"});
+        }
+        //return res.json(products);
+        prediction = await predict(userPurchases, products);
+        return res.json(prediction);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({"error":"Problem getting products"})
+    }
+});
 
 module.exports = router;
